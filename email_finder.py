d154688a16644d17742149f1d11c62237f1b993b
Andriy Rukas <rukasav@gmail.com>
2024-03-26 16:55:45 +0200

    Add venv and main function handle warnings

--- email_finder.py
+++ email_finder.py
@@ -3,6 +3,22 @@ import requests
 import requests.exceptions
 from urllib.parse import urlsplit
 from bs4 import BeautifulSoup
+import warnings
+from bs4 import GuessedAtParserWarning, MarkupResemblesLocatorWarning
+
+
+def main():
+    warnings.filterwarnings('ignore', category=GuessedAtParserWarning)
+    warnings.filterwarnings('ignore', category=MarkupResemblesLocatorWarning)
+
+    url = "https://www.scrapethissite.com/"
+
+    emails = get_emails(url)
+
+    print(f"Number of emails = {len(emails)}")
+    for email in emails:
+        print(print(email))
+
 
 def get_emails(url):
     urls = []
@@ -16,11 +32,12 @@ def get_emails(url):
             response = requests.get(next_url)
         except (requests.exceptions.MissingSchema, requests.exceptions.ConnectionError):
             continue
-        print(f"Crawling URL {next_url} ===================")
+        print(f"Crawling URL =================== {next_url} ")
         soup = BeautifulSoup(response.content, 'html.parser')
         links = soup.find_all("a")
-        new_emails = set(re.findall(r"[a-z0-9\.\-+_]+@[a-z0-9\.\-+_]+\.[a-z]+", response.text, re.I))
-       
+        new_emails = set(re.findall(
+            r"[a-z0-9\.\-+_]+@[a-z0-9\.\-+_]+\.[a-z]+", response.text, re.I))
+
         if len(new_emails) > 0:
             print(f"Found: {new_emails}")
             emails.update(new_emails)
@@ -29,26 +46,22 @@ def get_emails(url):
             if link.get("href"):
 
                 if ((next_url not in link.get("href")) & ("http" not in link.get("href")) & ("www" not in link.get("href"))):
-                    page_url = base_url.strip("/") + "/" + link.get("href").strip("/")
+                    page_url = base_url.strip(
+                        "/") + "/" + link.get("href").strip("/")
                     if page_url not in urls:
                         urls.append(page_url)
-                        print(f"Adding: {page_url} Number Added: {len(urls)}")
-                        
+                        print(f"Adding page # {len(urls)}: {page_url}")
+
                 elif (next_url in link.get("href")):
                     page_url = link.get("href")
                     if page_url not in urls:
-                       urls.append(page_url)
-                       print(f"Adding: {page_url} Number Added: {len(urls)}")
+                        urls.append(page_url)
+                        print(f"Adding: {page_url} Number Added: {len(urls)}")
     print(f"Total number of URLs:   {len(urls)}")
     print(f"Total number of Emails: {len(emails)}")
-    
+
     return emails
 
 
-
-url = "https://www.scrapethissite.com/"
-emails = get_emails(url)
-
-print(f"Number of emails = {len(emails)}")
-for email in emails:
-    print(print(email))
+if __name__ == "__main__":
+    main()
