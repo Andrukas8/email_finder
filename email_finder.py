5adfa2c28ab4dc1cc778ad17dacba0f9b05fc5ca
Andriy Rukas <rukasav@gmail.com>
2024-03-26 16:26:51 +0200

    Refactoring make crawling while scanning

--- email_finder.py
+++ email_finder.py
@@ -4,18 +4,26 @@ import requests.exceptions
 from urllib.parse import urlsplit
 from bs4 import BeautifulSoup
 
-
-def get_links(url):
+def get_emails(url):
     urls = []
-
+    emails = set()
     base_url = url
     if base_url not in urls:
         urls.append(base_url)
 
     for next_url in urls:
-        response = requests.get(next_url)
+        try:
+            response = requests.get(next_url)
+        except (requests.exceptions.MissingSchema, requests.exceptions.ConnectionError):
+            continue
+        print(f"Crawling URL {next_url} ===================")
         soup = BeautifulSoup(response.content, 'html.parser')
         links = soup.find_all("a")
+        new_emails = set(re.findall(r"[a-z0-9\.\-+_]+@[a-z0-9\.\-+_]+\.[a-z]+", response.text, re.I))
+       
+        if len(new_emails) > 0:
+            print(f"Found: {new_emails}")
+            emails.update(new_emails)
 
         for link in links:
             if link.get("href"):
@@ -31,33 +39,15 @@ def get_links(url):
                     if page_url not in urls:
                        urls.append(page_url)
                        print(f"Adding: {page_url} Number Added: {len(urls)}")
-
-
-
-    print(f"Total number of URLs: {len(urls)}")
-    return urls
-
-
-def get_emails(urls):
-    emails = set()
-    print("Searching for emails started...")
-    while len(urls):
-        url = urls.pop()
-        print(f"Crawling URL {url}")
-        try:
-            response = requests.get(url)
-        except (requests.exceptions.MissingSchema, requests.exceptions.ConnectionError):
-            continue
-
-        new_emails = set(re.findall(
-            r"[a-z0-9\.\-+_]+@[a-z0-9\.\-+_]+\.[a-z]+", response.text, re.I))
-        print(f"Found: {new_emails}")
-        emails.update(new_emails)
+    print(f"Total number of URLs:   {len(urls)}")
+    print(f"Total number of Emails: {len(emails)}")
+    
     return emails
 
 
-url = "http://www.hrid-andt.hr/"
-emails = get_emails(get_links(url))
+
+url = "https://www.scrapethissite.com/"
+emails = get_emails(url)
 
 print(f"Number of emails = {len(emails)}")
 for email in emails:
